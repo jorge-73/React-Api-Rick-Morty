2fb75c2d73da9dd4efea45ec0ba6ebd16027f2ce
jorge-73 <jorge_ap_73@hotmail.com>
2023-01-27 20:42:12 -0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://<jorge-73>.github.io/<React-Api-Rick-Morty>/"
+  base: "/React-Api-Rick-Morty/"
 })
